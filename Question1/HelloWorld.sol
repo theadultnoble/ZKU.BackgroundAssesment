ce1433caf6f29f9a4b20433b8008356301a17789
Noble <okafornoble202@gmail.com>
2022-03-02 00:21:09 +0100

    second

--- Question1/HelloWorld.sol
+++ Question1/HelloWorld.sol
@@ -2,6 +2,15 @@
 // compiler version must be greater than or equal to 0.8.10 and less than 0.9.0
 pragma solidity ^0.8.10;
 
+//Declares Contract HelloWorld.
 contract HelloWorld {
-    string public greet = "Hello World!";
+    ///declares Var "count" & sets initial value.
+    uint public count = 5;
+
+
+    // Function to get the current count.
+    function getCount() view public returns (uint) {
+        return count;
+    }
+
 }
